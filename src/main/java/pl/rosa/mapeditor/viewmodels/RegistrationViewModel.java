c6a0d6f2e5450ca3e52daa01c4fe078dffcc9491
Maciej <macrosa94@gmail.com>
2018-10-25 09:56:22 +0200

    Updated email constraint.

--- src/main/java/pl/rosa/mapeditor/viewmodels/RegistrationViewModel.java
+++ src/main/java/pl/rosa/mapeditor/viewmodels/RegistrationViewModel.java
@@ -2,14 +2,14 @@ package pl.rosa.mapeditor.viewmodels;
 
 import de.malkusch.validation.constraints.EqualProperties;
 
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.Pattern;
 
 @EqualProperties(value = {"password","repeatPassword"}, message = "{Passwords.dontMatch}")
 public class RegistrationViewModel {
 
     @NotEmpty(message = "{NotEmpty.message}")
-    @Pattern(regexp = "^[_A-Za-z0-9-]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9-+]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$", message = "{Email.invalidEmail}")
+    @Email(message = "{Email.invalidEmail}")
     private String email;
 
     @NotEmpty(message = "{NotEmpty.message}")
