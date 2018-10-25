2f27a7e73ddbbc7c14de354e99e817e58cea002a
Maciej <macrosa94@gmail.com>
2018-10-25 10:46:20 +0200

    Added map and map access tables to database.

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -4,3 +4,18 @@ CREATE TABLE IF NOT EXISTS appuser(
   password text NOT NULL,
   name text NOT NULL
 );
+
+CREATE TABLE IF NOT EXISTS map(
+  id SERIAL PRIMARY KEY,
+  name text NOT NULL,
+  description text,
+  visibility text NOT NULL,
+  owner_id INTEGER REFERENCES appuser(id) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS map_access(
+  id SERIAL PRIMARY KEY,
+  access_type text NOT NULL,
+  map_id INTEGER REFERENCES map(id) NOT NULL,
+  app_user_id INTEGER REFERENCES appuser(id) NOT NULL
+);
