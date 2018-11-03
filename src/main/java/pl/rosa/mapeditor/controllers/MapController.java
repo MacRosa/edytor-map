612cbc4f1a138efb2dbebd646f8dac3dcbeecf14
Maciej <macrosa94@gmail.com>
2018-11-03 11:13:10 +0100

    Passing flash attribute about successfully added map to list of maps.

--- src/main/java/pl/rosa/mapeditor/controllers/MapController.java
+++ src/main/java/pl/rosa/mapeditor/controllers/MapController.java
@@ -7,7 +7,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import pl.rosa.mapeditor.exceptions.AppUserNotLoggedInException;
+import pl.rosa.mapeditor.login.LoggedUser;
+import pl.rosa.mapeditor.models.AppUser;
 import pl.rosa.mapeditor.services.MapService;
 import pl.rosa.mapeditor.viewmodels.MapViewModel;
 
@@ -20,16 +23,27 @@ import javax.validation.Valid;
 public class MapController {
 
     private MapService mapService;
+    private LoggedUser loggedUser;
 
     @Autowired
-    public MapController(MapService mapService) {
+    public MapController(MapService mapService, LoggedUser loggedUser) {
         this.mapService = mapService;
+        this.loggedUser = loggedUser;
     }
 
 
     @GetMapping("/map/list")
-    public ModelAndView listMaps(){
-        return new ModelAndView("maplist");
+    public ModelAndView listMaps(@ModelAttribute("addmap")String addmap){
+        ModelAndView modelAndView = new ModelAndView("maplist");
+
+        AppUser user = loggedUser.getLoggedUser().getAppUser();
+
+        modelAndView.addObject("ownedmaps",user.getOwnedMaps());
+
+        if(addmap.equals("mapadded")){
+            modelAndView.addObject("mapadded",true);
+        }
+        return modelAndView;
     }
 
     @GetMapping("/map/add")
@@ -38,7 +52,8 @@ public class MapController {
     }
 
     @PostMapping("/map/add")
-    public ModelAndView addingMap(@Valid @ModelAttribute("model")MapViewModel model,final BindingResult result){
+    public ModelAndView addingMap(@Valid @ModelAttribute("model")MapViewModel model,final BindingResult result,
+                                    final RedirectAttributes redirectAttributes){
         if(result.hasErrors()){
             return new ModelAndView("addmap","model",model);
         }
@@ -48,6 +63,8 @@ public class MapController {
             ex.printStackTrace();
             return new ModelAndView("redirect:/login");
         }
+
+        redirectAttributes.addFlashAttribute("addmap","mapadded");
 
         return new ModelAndView("redirect:/map/list");
     }
