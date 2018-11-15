b82371a93770755e9f3995a2aedc71999cc11e13
Maciej <macrosa94@gmail.com>
2018-11-15 10:07:15 +0100

    Removed test controller.

--- src/main/java/pl/rosa/mapeditor/controllers/TestController.java
+++ src/main/java/pl/rosa/mapeditor/controllers/TestController.java
@@ -6,19 +6,23 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import pl.rosa.mapeditor.models.AppUser;
+import pl.rosa.mapeditor.models.map.MapDetails;
 import pl.rosa.mapeditor.repositories.AppUserRepository;
+import pl.rosa.mapeditor.repositories.MapDetailsRepository;
 
 import java.util.Optional;
+import java.util.Random;
 
 @Controller
 public class TestController {
 
     final private AppUserRepository appUserRepository;
-
+    private MapDetailsRepository mapDetailsRepository;
 
     @Autowired
-    public TestController(AppUserRepository appUserRepository) {
+    public TestController(AppUserRepository appUserRepository, MapDetailsRepository mapDetailsRepository) {
         this.appUserRepository = appUserRepository;
+        this.mapDetailsRepository = mapDetailsRepository;
     }
 
     private String userToString(AppUser user){
@@ -43,6 +47,17 @@ public class TestController {
         if(user == null)
             return "User not found";
         return userToString(user);
+    }
+
+    @RequestMapping("/test/mapdetails/create")
+    @ResponseBody
+    public String createMapDetails(){
+        MapDetails details = new MapDetails();
+        Random random = new Random();
+        details.setHeight(random.nextDouble());
+        details.setWidth(random.nextDouble());
+        mapDetailsRepository.save(details);
+        return "Create";
     }
 
 
