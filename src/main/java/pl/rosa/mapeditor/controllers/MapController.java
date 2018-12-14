aefa26699db926cb46fc265e72bc11e357c216dc
Maciej <macrosa94@gmail.com>
2018-12-14 18:44:16 +0100

    * Added map controller method for receiving map as SVG image. * Added permission for all to get map as SVG image.

--- src/main/java/pl/rosa/mapeditor/controllers/MapController.java
+++ src/main/java/pl/rosa/mapeditor/controllers/MapController.java
@@ -20,9 +20,11 @@ import pl.rosa.mapeditor.services.AppUserService;
 import pl.rosa.mapeditor.services.MapService;
 import pl.rosa.mapeditor.utils.JSONToMapConverter;
 import pl.rosa.mapeditor.utils.MapDetailsRequest;
+import pl.rosa.mapeditor.utils.MapToSVGConverter;
 import pl.rosa.mapeditor.viewmodels.MapViewModel;
 
 import javax.validation.Valid;
+import javax.xml.transform.TransformerException;
 import java.io.IOException;
 
 /**
@@ -37,108 +39,110 @@ public class MapController {
     private final JSONToMapConverter mapConverter;
     private ObjectMapper objectMapper;
     private MapDetailsRepository mapDetailsRepository;
+    private MapToSVGConverter mapToSVGConverter;
 
     @Autowired
-    public MapController(MapService mapService, LoggedUser loggedUser, AppUserService appUserService, JSONToMapConverter mapConverter, MapDetailsRepository mapDetailsRepository) {
+    public MapController(MapService mapService, LoggedUser loggedUser, AppUserService appUserService, JSONToMapConverter mapConverter, MapDetailsRepository mapDetailsRepository, MapToSVGConverter mapToSVGConverter) {
         this.mapService = mapService;
         this.loggedUser = loggedUser;
         this.appUserService = appUserService;
         this.mapConverter = mapConverter;
         this.mapDetailsRepository = mapDetailsRepository;
+        this.mapToSVGConverter = mapToSVGConverter;
         this.objectMapper = new ObjectMapper();
     }
 
 
     @GetMapping("/map/list")
-    public ModelAndView listMaps(@ModelAttribute("addmap")String addmap){
+    public ModelAndView listMaps(@ModelAttribute("addmap") String addmap) {
         ModelAndView modelAndView = new ModelAndView("maplist");
 
         AppUser user = appUserService.getAppUser(loggedUser.getLoggedUser());
-        modelAndView.addObject("ownedmaps",user.getOwnedMaps());
+        modelAndView.addObject("ownedmaps", user.getOwnedMaps());
 
-        if(addmap.equals("mapadded")){
-            modelAndView.addObject("mapadded",true);
+        if (addmap.equals("mapadded")) {
+            modelAndView.addObject("mapadded", true);
         }
         return modelAndView;
     }
 
     @GetMapping("/map/add")
     public ModelAndView addMap() {
-        return new ModelAndView("addmap","model", new MapViewModel());
+        return new ModelAndView("addmap", "model", new MapViewModel());
     }
 
     @PostMapping("/map/add")
-    public ModelAndView addingMap(@Valid @ModelAttribute("model")MapViewModel model,final BindingResult result,
-                                    final RedirectAttributes redirectAttributes){
-        if(result.hasErrors()){
-            return new ModelAndView("addmap","model",model);
+    public ModelAndView addingMap(@Valid @ModelAttribute("model") MapViewModel model, final BindingResult result,
+                                  final RedirectAttributes redirectAttributes) {
+        if (result.hasErrors()) {
+            return new ModelAndView("addmap", "model", model);
         }
-        try{
+        try {
             mapService.addMap(model);
-        }catch(AppUserNotLoggedInException ex){
+        } catch (AppUserNotLoggedInException ex) {
             ex.printStackTrace();
             return new ModelAndView("redirect:/login");
         }
 
-        redirectAttributes.addFlashAttribute("addmap","mapadded");
+        redirectAttributes.addFlashAttribute("addmap", "mapadded");
 
         return new ModelAndView("redirect:/map/list");
     }
 
-    private Map addMapModelAndValidate(ModelAndView modelAndView,Long id){
-        modelAndView.addObject("notfound",false);
-        modelAndView.addObject("noaccess",false);
-        try{
+    private Map addMapModelAndValidate(ModelAndView modelAndView, Long id) {
+        modelAndView.addObject("notfound", false);
+        modelAndView.addObject("noaccess", false);
+        try {
             Map map = mapService.getMap(id);
-            modelAndView.addObject("model",map);
+            modelAndView.addObject("model", map);
             return map;
-        }catch(MapNotFoundException ex){
-            modelAndView.addObject("notfound",true);
-        }catch(NoAccessToMapException ex){
-            modelAndView.addObject("noaccess",true);
+        } catch (MapNotFoundException ex) {
+            modelAndView.addObject("notfound", true);
+        } catch (NoAccessToMapException ex) {
+            modelAndView.addObject("noaccess", true);
         }
         return null;
     }
 
 
     @GetMapping("/map/show/{id}")
-    public ModelAndView getMap(@PathVariable("id")Long id){
+    public ModelAndView getMap(@PathVariable("id") Long id) {
         ModelAndView modelAndView = new ModelAndView("showmap");
-        addMapModelAndValidate(modelAndView,id);
+        addMapModelAndValidate(modelAndView, id);
         return modelAndView;
     }
 
     @GetMapping("/map/edit/{id}")
-    public ModelAndView editMap(@PathVariable("id")Long id){
+    public ModelAndView editMap(@PathVariable("id") Long id) {
         ModelAndView modelAndView = new ModelAndView("map_editor");
-        Map map = addMapModelAndValidate(modelAndView,id);
-        if(map != null){
-            modelAndView.addObject("cantedit",!(mapService.currentUserCanEdit(map)));
-        }else{
-            modelAndView.addObject("cantedit",true);
+        Map map = addMapModelAndValidate(modelAndView, id);
+        if (map != null) {
+            modelAndView.addObject("cantedit", !(mapService.currentUserCanEdit(map)));
+        } else {
+            modelAndView.addObject("cantedit", true);
         }
         return modelAndView;
     }
 
     @PostMapping("/map/edit/{id}")
     @Transactional
-    public ModelAndView sendEditedMap(@PathVariable("id")Long id, @RequestParam("mapData") String mapData) throws IOException {
+    public ModelAndView sendEditedMap(@PathVariable("id") Long id, @RequestParam("mapData") String mapData) throws IOException {
         try {
             ModelAndView modelAndView = new ModelAndView("redirect:/map/show/" + id);
             Map map = mapService.getMap(id);
-            if(!mapService.currentUserCanEdit(map)){
+            if (!mapService.currentUserCanEdit(map)) {
                 return new ModelAndView("error");
             }
             MapDetails mapDetails = mapConverter.getMapFromJson(objectMapper.readTree(mapData));
-            if(map.getDocumentId() == null) {
+            if (map.getDocumentId() == null) {
                 mapDetailsRepository.save(mapDetails);
                 map.setDocumentId(mapDetails.getId());
-            }else{
+            } else {
                 MapDetails oldDetails = mapDetailsRepository.findById(map.getDocumentId()).orElse(null);
-                if(oldDetails == null){
+                if (oldDetails == null) {
                     mapDetailsRepository.save(mapDetails);
                     map.setDocumentId(mapDetails.getId());
-                }else{
+                } else {
                     mapDetails.setId(oldDetails.getId());
                     mapDetailsRepository.save(mapDetails);
                 }
@@ -156,26 +160,44 @@ public class MapController {
 
     @GetMapping("/map/details/{id}")
     @ResponseBody
-    public MapDetailsRequest getMapDetails(@PathVariable("id")Long id){
+    public MapDetailsRequest getMapDetails(@PathVariable("id") Long id) {
         try {
             Map map = mapService.getMap(id);
-            if(!mapService.currentUserCanEdit(map)){
-                return new MapDetailsRequest(false,"No permissions to edit",null);
+            if (!mapService.currentUserCanEdit(map)) {
+                return new MapDetailsRequest(false, "No permissions to edit", null);
             }
-            if(map.getDocumentId() == null){
-                return new MapDetailsRequest(true,"No document",null);
+            if (map.getDocumentId() == null) {
+                return new MapDetailsRequest(true, "No document", null);
             }
 
             MapDetails mapDetails = mapDetailsRepository.findById(map.getDocumentId()).orElse(null);
-            if(mapDetails == null){
-                return new MapDetailsRequest(true,"No details",null);
+            if (mapDetails == null) {
+                return new MapDetailsRequest(true, "No details", null);
             }
-            return new MapDetailsRequest(true,"success",mapDetails);
+            return new MapDetailsRequest(true, "success", mapDetails);
         } catch (MapNotFoundException e) {
-            return new MapDetailsRequest(false,"Map not found",null);
+            return new MapDetailsRequest(false, "Map not found", null);
         } catch (NoAccessToMapException e) {
-            return new MapDetailsRequest(false,"No access to map",null);
+            return new MapDetailsRequest(false, "No access to map", null);
         }
     }
 
+    @GetMapping(value = "/map/svg/{id}",
+            produces = "image/svg+xml")
+    @ResponseBody
+    public byte[] getMapSVGImage(@PathVariable("id") Long id) {
+        try {
+            Map map = mapService.getMap(id);
+            if (map.getDocumentId() == null) {
+                return new byte[0];
+            }
+            MapDetails mapDetails = mapDetailsRepository.findById(map.getDocumentId()).orElse(null);
+            if (mapDetails == null) {
+                return new byte[0];
+            }
+            return mapToSVGConverter.toByteSVGImage(mapDetails);
+        } catch (MapNotFoundException | NoAccessToMapException | TransformerException e) {
+            return new byte[0];
+        }
+    }
 }
