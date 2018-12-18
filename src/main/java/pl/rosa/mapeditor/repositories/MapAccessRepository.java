58da204e40dc0d54fd81f4d6c674e4b12939668f
Maciej <macrosa94@gmail.com>
2018-12-18 09:40:43 +0100

    Added finding map access by user and map id.

--- src/main/java/pl/rosa/mapeditor/repositories/MapAccessRepository.java
+++ src/main/java/pl/rosa/mapeditor/repositories/MapAccessRepository.java
@@ -4,9 +4,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import pl.rosa.mapeditor.models.MapAccess;
 
+import java.util.Optional;
+
 /**
  * Created by Maciej on 2018-12-17 15:58
  */
 @Repository
 public interface MapAccessRepository extends JpaRepository<MapAccess, Long> {
+
+    Optional<MapAccess> findByMapIdAndAppUserId(Long mapId,Long appUserId);
 }
